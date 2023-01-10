ca1f53af081667d5c5a277c8ecb0f195d673e35e
Sir-HAKASE <tusima.tasyuyan@gmail.com>
2023-01-10 13:45:18 +0900

    3rd commit

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4439", "message": "なんだ？これは！"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4439", "message": "イヤホンは沼"})
     }
 }
